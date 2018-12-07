d47d74204874430f8baed797a667e57616746417
abyarth7 <abyarth.behera@pharmeasy.in>
2018-12-07 15:48:06 +0530

    alembic and migration auto generation setup

---
+++ app/alembic/versions/9fe7aae51e09_create_account_table.py
@@ -0,0 +1,24 @@
+"""create account table
+
+Revision ID: 9fe7aae51e09
+Revises: f7ecad1a2b88
+Create Date: 2018-12-07 15:40:06.851744
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9fe7aae51e09'
+down_revision = 'f7ecad1a2b88'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
